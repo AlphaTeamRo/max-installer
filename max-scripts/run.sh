a2e35c973ad6a39605992e6ef7d42a4f23264acb
Mihai Cristian <59600013+CristianEduardMihai@users.noreply.github.com>
2022-09-10 19:01:00 +0300

    trying to fix the one liner

--- max-scripts/run.sh
+++ max-scripts/run.sh
@@ -1,6 +1,4 @@
 sudo apt -y install wget
 mkdir max-scripts
 wget https://raw.githubusercontent.com/AlphaTeamRo/max-installer/main/max-scripts/setup.sh -P max-scripts
-cd max-scripts
-chmod +x setup.sh
-sudo ./setup.sh
+chmod +x max-scripts/setup.sh
